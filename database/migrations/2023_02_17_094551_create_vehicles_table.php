13412ceeb39f3629def0e687319c94812356bae1
ALLY-MAFTAH <maflampizzle69@gmail.com>
2023-03-18 23:20:49 +0300

    Added bootstrap UI component and auth

---
+++ database/migrations/2023_02_17_094551_create_vehicles_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vehicles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('vehicle_type_id');
+            $table->string('number')->unique();
+            $table->string('owner');
+            $table->string('brand');
+            $table->string('color')->nullable();
+            $table->string('parking_id');
+            $table->date('registered_date');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vehicles');
+    }
+};
